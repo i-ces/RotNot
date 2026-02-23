eb2365bc5ad31c159b067e8681f6ea25730d4ad2
GitWitAnish <anishkarki5710@gmail.com>
2026-02-24 04:13:38 +0545

    final commit

--- backend/src/middlewares/notFound.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-
-export const notFoundHandler = (req: Request, res: Response, _next: NextFunction) => {
-  res.status(404).json({
-    success: false,
-    message: `Route ${req.originalUrl} not found`,
-  });
-};
